85adb3a8d568cb94e36dfab7a5ae1df649e967b3
Sergey Mudrik <sergey.mudrik@gmail.com>
2016-02-27 23:41:20 +0200

    Fixed test

--- go-carpet_test.go
+++ go-carpet_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func Test_readFileByLines(t *testing.T) {
-	file, err := readFile("go-cover-cli_test.go")
+	file, err := readFile("go-carpet_test.go")
 	if err != nil {
 		t.Errorf("Got error: %s", err)
 	}
