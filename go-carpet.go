c4ea236cfb6c25b0de6934bbb5faf56a4f558c48
Sergey Mudrik <sergey.mudrik@gmail.com>
2016-02-28 12:26:27 +0200

    Moved docs

--- go-carpet.go
+++ go-carpet.go
@@ -1,23 +1,3 @@
-/*
-go-carpet - show test coverage for Go source files
-
-It works not only in the directory GOPATH. And it works recursively for multiple packages.
-
-Install/update:
-
-	go get -u github.com/msoap/go-carpet
-	ln -s $GOPATH/bin/go-carpet ~/bin/go-carpet
-
-Usage:
-
-	go-carpet [-options] [paths]
-	options:
-		-256colors - use more colors on 256-color terminal
-		-file string - comma separated list of files to test (defualt: all)
-
-Source: https://github.com/msoap/go-carpet
-
-*/
 package main
 
 import (
