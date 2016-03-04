2bd9bd220e07a72b51b61e6e885c362189bd3bb0
Sergey Mudrik <sergey.mudrik@gmail.com>
2016-03-04 21:41:28 +0200

    Added test for isSliceInStringPrefix()

--- go-carpet_test.go
+++ go-carpet_test.go
@@ -116,7 +116,43 @@ func Test_isSliceInString(t *testing.T) {
 	for i, item := range testData {
 		result := isSliceInString(item.src, item.slice)
 		if result != item.result {
-			t.Errorf("\n%d.\nexpected: %v\nreal    :%v", i, item.result, result)
+			t.Errorf("\n%d. isSliceInString()\nexpected: %v\nreal    :%v", i, item.result, result)
+		}
+	}
+}
+
+func Test_isSliceInStringPrefix(t *testing.T) {
+	testData := []struct {
+		src    string
+		slice  []string
+		result bool
+	}{
+		{
+			src:    "one/file.go",
+			slice:  []string{"vendor", "Godeps"},
+			result: false,
+		},
+		{
+			src:    "vendor/path/file.go",
+			slice:  []string{"vendor", "Godeps"},
+			result: true,
+		},
+		{
+			src:    "Godeps/path/file.go",
+			slice:  []string{"vendor", "Godeps"},
+			result: true,
+		},
+		{
+			src:    "one/file.go",
+			slice:  []string{},
+			result: false,
+		},
+	}
+
+	for i, item := range testData {
+		result := isSliceInStringPrefix(item.src, item.slice)
+		if result != item.result {
+			t.Errorf("\n%d. isSliceInStringPrefix()\nexpected: %v\nreal     :%v", i, item.result, result)
 		}
 	}
 }
